487e4ba65ce20b2ff681c7f32cc15c6a2d0a04d9
NamerM <mnamer12@outlook.com>
2023-03-01 16:00:35 +0200

    First component header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useLocation} from 'react-dom';
+
+
+function Header (){
+
+
+
+  return (
+    <header className="header">
+
+
+    </header>
+  )
+}
+export default Header;
